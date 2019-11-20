fbd25cf5e69a94990a99391c6e7b15d3e79096f9
rajatarora21 <arorarajat9999@gmail.com>
2019-11-19 20:43:59 -0500

    Data File handling

--- Assignment-4/Q1.c
+++ Assignment-4/Q1.c
@@ -1,15 +1,22 @@
 #include<stdio.h>
-int fun(int n)
-{
-   if(n==0)
-	   return 1;
-   return (n*fun(n-1));
-
-}
 int main()
 {
-	int x=fun(3);
-	printf("\n\n%d\n\n",x);
-  	printf("hello there");
+	FILE *filepointer;
+	filepointer = fopen("sample.txt","r");
+	if(filepointer==NULL)
+	{
+		printf("\n\nGet lost\n\n");
+	}
+	else
+	{
+		printf("\n\nThe file has been openend\n\n");
+		char value[100];
+		while(fgets(value,100,filepointer)!=NULL)
+		{
+			printf("%s",value);
+		}
+		printf("you are done");
+	}	
+	fclose(filepointer);
 	return 0;
 }
