c29af206a6e009ed9f400e7fc39f0efc16630566
rajatarora21 <arorarajat9999@gmail.com>
2019-11-20 16:41:16 -0500

    Removed all the comments
    Removed all the comments from the source code and displayed the resulting code on the console

--- Assignment-4/Q1.c
+++ Assignment-4/Q1.c
@@ -13,33 +13,50 @@ int main()
 		printf("\n\nThe file has been openend\n\n");
 		int tempValue=0;
 		int tempValue2=0;
+		int tempValue3=0;
 		char value[100];
 		char c;
 		while(1)
 		{
-			
 			c=fgetc(filepointer);
 			if(c==EOF)
 				break;
-			if(tempValue!=1)
+			else if(c=='"')
 			{
-				if(c=='/'&&fgetc(filepointer)=='/')
-					tempValue2=1;
+				if(tempValue3==0)
+					tempValue3=1;
+				else
+					tempValue3=0;
 			}
-			if(tempValue2)
-			else if(c=='/'&&fgetc(filepointer)=='/'&&tempValue==0)
-				tempValue2=1;
-			else if(c=='/'&&fgetc(filepointer)=='*'tempValue2==0)
-				tempValue=1;
-			else if(tempValue==1&&c=='*'&&fgetc(filepointer)=='/')
-				tempValue=0;
-			else if(tempValue2==1&&c=='\n')
-				tempValue2=0;
-			else if(tempValue==0&&tempValue2==0)
+			if(tempValue3==0)
+			{
+				 if(c=='/')
+				{
+					c=fgetc(filepointer);
+					if(c=='*')
+						tempValue=1;
+					else if(c=='/')
+						tempValue2=1;
+				}	
+				else if(c=='*')
+				{
+					c=fgetc(filepointer);
+					if(c=='/')
+						tempValue=0;
+				}
+				else if((c=='\n')&&(tempValue2==1))
+					tempValue2=0;
+
+				else if(tempValue==0&&tempValue2==0)
+					printf("%c",c);
+			}
+			else 
 				printf("%c",c);
 
+			
 
 		}
+		
 		printf("\n\nyou are done\n\n");
 	}	
 	fclose(filepointer);
--- Assignment-4/sample.c
+++ Assignment-4/sample.c
@@ -1,10 +1,16 @@
 #include<stdio.h>
-int main()
-{
-	//hello there
-	//fuck you bro*/
-	printf("hello fucker");//dsdsds
+int main(){
+
+	/*hello there*/
+	//hey thetere
+	static char* s = "/*This is not a comment!*/";//hey theere
+	printf("hello there");/*hehehe*///ehhehehhe
 	return 0;
-	/*sdaf*/
+
+	/* the following code is commented out
+ int a = 12; // nested comment
+ /* // nested nested comment
+ int b = a++; */
+*/
 	
 }
