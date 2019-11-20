ed84f04861524cd774fe61c8234b86b61707877d
rajatarora21 <arorarajat9999@gmail.com>
2019-11-19 22:12:40 -0500

    Remove comments from c file
    Used data file handling in c to remove multi-line comments

--- Assignment-4/Q1.c
+++ Assignment-4/Q1.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
 	FILE *filepointer;
-	filepointer = fopen("sample.txt","r");
+	filepointer = fopen("sample.c","r");
 	if(filepointer==NULL)
 	{
 		printf("\n\nGet lost\n\n");
@@ -10,10 +11,23 @@ int main()
 	else
 	{
 		printf("\n\nThe file has been openend\n\n");
+		int tempValue=0;
 		char value[100];
-		while(fgets(value,100,filepointer)!=NULL)
+		char c;
+		while(1)
 		{
-			printf("%s",value);
+			
+			c=fgetc(filepointer);
+			if(c==EOF)
+				break;
+			else if(c=='/'&&fgetc(filepointer)=='*')
+				tempValue=1;
+			else if(tempValue==1&&c=='*'&&fgetc(filepointer)=='/')
+				tempValue=0;
+			else if(tempValue==0)
+				printf("%c",c);
+
+
 		}
 		printf("you are done");
 	}	
---
+++ Assignment-4/sample.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+	/*hello there*/
+	printf("hello fucker");
+	return 0;
+}
